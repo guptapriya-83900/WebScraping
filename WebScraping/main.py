8352986039314ff746f9d8f1a8a786cbb5c34a2d
Priya Gupta <guptapriya83900@gmail.com>
2025-01-04 14:15:30 -0800

    Onetime_Scraping_Done

--- WebScraping/main.py
+++ WebScraping/main.py
@@ -4,6 +4,11 @@ import pandas as pd
 
 name=[]
 required_skills=[]
+info=[]
+# print('Put some skills that you are not familiar with')
+# unfamiliar_skill=input('>').split(',')
+# print(f"Filtering out {unfamiliar_skill}")
+
 html_text = requests.get(
     "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=as&searchTextText=Python&txtKeywords=Python&txtLocation="
     ).text
@@ -27,20 +32,27 @@ for job in job_cards:
         skills = job.find('div', class_='more-skills-sections').text.strip()
         skills = ' '.join(skills.split())  # Remove multiple spaces and clean the string
         skills = skills.replace(' ', ', ')  # Ensure proper spacing after commas
+        more_info=job.find('a')['href']
+        # more_info=job.a['href']      ---Another way of extracting detail
+        # Check if none of the unfamiliar skills are in the skills list. This is for getting only the required skills job
+        # if not any(remove_skills.casefold() in skills.casefold() for remove_skills in unfamiliar_skill):
+        #     print(f"Company Name: {company_name}")
+        #     print(f"Required Skills: {skills}")
+        #     print(f"More info: {more_info}")
+        #     print("\n")
         name.append(company_name)
         required_skills.append(skills)
+        info.append(more_info)
+       
+
+    
 
 
-data = pd.DataFrame({'Company_Name':name,'Skills':required_skills})
+data = pd.DataFrame({'Company_Name':name,'Skills':required_skills, 'More_info':info})
 print(data.head())
 
-data.to_csv('jobs.csv',index=False,header=True)
-
-    # print(f'''
-    # Company Name: {company_name}
-    # Required Skills: {skills}
-    # ''')
+#data.to_csv('jobs.csv',index=False,header=True)
 
 
-#print(job_cards)
+
 
